cdbaf26dfcba3bb3cfc6f23ac84e3bfeb07a29d0
BenteKavsek <35726@ma-web.nl>
2023-12-01 13:19:07 +0100

    2.5 af

--- m6-prog_project/public/search.php
+++ m6-prog_project/public/search.php
@@ -1,6 +1,30 @@
 <?php
+
+require_once '../source/config.php';
+require_once SOURCE_ROOT . 'database.php';
+
+$connection = database_connect();
+
+$plaats = $_GET ['plaats'];
+
+$sql = 'SELECT * FROM weersomstandighedenPerDag WHERE Plaats=? ORDER BY Datum'; 
+
+$stmt = $connection->prepare($sql);
+
+$stmt->bind_param('s', $plaats);
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+while ( $weersomstandigheden = mysqli_fetch_assoc($result)){
     echo '<pre>';
     var_dump( $_GET );
     echo '</pre>';
+};
+
+var_dump( $weersomstandigheden );
+
+
 
 ?>
