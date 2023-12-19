8574e6363a9f9b6d2cb8bc64c520243a8924fa70
BenteKavsek <35726@ma-web.nl>
2023-12-19 10:07:14 +0100

    4.1 naw table gemaakt

---
+++ m6-prog_project/source/02 naw insert.sql
@@ -0,0 +1,7 @@
+use M6prog;
+
+insert into naw (naam,email,straat,huisnummer,postcode);
+values
+("leraar","leraar@ma-web.nl","contactweg","1","1100AA"),
+("leraar2","leraar2@ma-web.nl","contactweg","1","1100AA"),
+("leraar3","leraar3@ma-web.nl","contactweg","1","1100AA");
